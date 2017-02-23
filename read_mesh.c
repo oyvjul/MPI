4f0f12afe08f491b76bcd35fd24ae98b1ade5b77
Øyvind Julsrud <oyvjul@gmail.com>
2017-02-23 15:32:48 +0100

    initialized cube with coordinates

--- read_mesh.c
+++ read_mesh.c
@@ -28,6 +28,50 @@ typedef struct
     int numnodes;
 } meshdata;
 
+typedef struct
+{
+  double x, y, z;
+
+} cube;
+
+double ***dallocate_3d(int x, int y, int z)
+{
+  int i, j;
+  double *storage = (double*)malloc(x * y * z * sizeof(*storage));
+  double *alloc = storage;
+  double ***matrix;
+  matrix = (double***)malloc(z * sizeof(double**));
+
+  for (i = 0; i < z; i++)
+  {
+    matrix[i] = (double**)malloc(y * sizeof(**matrix));
+
+    for (j = 0; j < y; j++)
+    {
+      matrix[i][j] = alloc;
+      alloc += x;
+    }
+  }
+
+  return matrix;
+}
+
+void dinit_3d(double*** matrix, int x, int y, int z)
+{
+  int i, j, k;
+
+  for(i = 0; i < z; i++)
+  {
+    for(j = 0; j < y; j++)
+    {
+      for(k = 0; k < x; k++)
+      {
+        matrix[i][j][k] = 0.0;
+      }
+    }
+  }
+}
+
 void readmesh(char* infile, meshdata *M)
 {
     char fname[200];
@@ -195,57 +239,61 @@ int main(int argc, char *argv[])
   double y_min;
   double z_max;
   double z_min;
+  double ***u_old, ***u_new;
   double det_0, det_1, det_2, det_3, det_4;
   const double constant = 1.0;
+  int x = 10;
+  int y = 10;
+  int z = 10;
+  double x_step, y_step, z_step;
+
+  cube *grid, point;
+  grid = calloc(x*y*z, sizeof(*grid));
 
   double a[3] = {1.1, 1.2, 1.3};
   double b[3] = {2.1, 2.2, 2.3};
   double c[3] = {3.1, 3.2, 3.3};
   double d[3] = {3.1, 3.2, 3.3};
 
-  double arr[5][5][5];
-
-  det_a = (a[0]*b[1]*c[2]*constant) + (a[0]*b[2]*constant*d[1]) + (a[0]*constant*c[1]*d[3])
+  /*det_a = (a[0]*b[1]*c[2]*constant) + (a[0]*b[2]*constant*d[1]) + (a[0]*constant*c[1]*d[3])
         + (a[1]*b[0]*constant*d[3]) + (a[1]*b[2]*c[0]*constant) + (a[1]*constant*c[2]*d[0])
         + (a[2]*b[0]*c[1]*constant) + (a[2]*b[1]*constant*d[0]) + (a[2]*constant*c[0]*d[1])
         + (constant*b[0]*c[2]*d[1]) + (constant*b[1]*c[0]*d[2]) + (constant*b[2]*c[1]*d[0])
-        - (a[0]*b[1]**constant*d[2]) - (a[0]*b[2]*constant*d[1]) - (a[0]*constant*c[1]*d[3])
+        //- (a[0]*b[1]*constant*d[2]) - (a[0]*b[2]*constant*d[1]) - (a[0]*constant*c[1]*d[3])
         - (a[1]*b[0]*constant*d[3]) - (a[1]*b[2]*c[0]*constant) - (a[1]*constant*c[2]*d[0])
         - (a[2]*b[0]*c[1]*constant) - (a[2]*b[1]*constant*d[0]) - (a[2]*constant*c[0]*d[1])
-        - (constant*b[0]*c[2]*d[1]) - (constant*b[1]*c[0]*d[2]) - (constant*b[2]*c[1]*d[0]):
+        - (constant*b[0]*c[2]*d[1]) - (constant*b[1]*c[0]*d[2]) - (constant*b[2]*c[1]*d[0]);*/
 
 
 
   readmesh("mesh_new/3Dheart.1", &m);
+  compute_minmax(&x_max, &x_min, &y_max, &y_min, &z_max, &z_min, &m);
 
-  /*for(i=0; i<20; i++)
+  x_step = (x_max - x_min)/(double)x;
+  y_step = (y_max - y_min)/(double)y;
+  z_step = (z_max - z_min)/(double)z;
+
+  printf("x_max: %f \t x_min: %f \t y_max: %f \t y_min: %f \t z_max: %f \t z_min: %f\n", x_max, x_min, y_max, y_min, z_max, z_min);
+  //printf("%f \n", step);
+  for(i = 0; i < z; i++)
   {
-      //compute indices to access nodes array
-      int A = m.elements[i*4]*3;
-      int B = m.elements[i*4+1]*3;
-      int C = m.elements[i*4+2]*3;
-      int D = m.elements[i*4+3]*3;
-
-      printf("A: %d \t B: %d \t C: %d \t D: %d \n", A, B, C, D);
-  }*/
-
-  //compute_minmax(&x_max, &x_min, &y_max, &y_min, &z_max, &z_min, &m);
-
-  //printf("x_max: %f \t x_min: %f \t y_max: %f \t y_min: %f \t z_max: %f \t z_min: %f\n", x_max, x_min, y_max, y_min, z_max, z_min);
-
-  for(i = 0; i < 5; i++)
-  {
-    for(j = 0; j < 5; j++)
+    for(j = 0; j < y; j++)
     {
-      for(k = 1; k < 5; k++)
+      for(k = 0; k < x; k++)
       {
-        arr[i][j][k] = a[k];
+        point.x = x_min + x_step*i;
+        point.y = y_min + y_step*j;
+        point.z = z_min + z_step*k;
+
+        grid[i*y*z+j*y+k] = point;
       }
     }
   }
 
+  printf("%f \n", grid[0].z);
 
-  for(i = 0; i < 5; i++)
+
+  /*for(i = 0; i < 5; i++)
   {
     for(j = 0; j < 5; j++)
     {
@@ -257,8 +305,8 @@ int main(int argc, char *argv[])
     }
     printf("\n");
   }
-  printf("\n");
+  printf("\n");*/
 
-  calculate_centroid(&m);
+  //calculate_centroid(&m);
 
 }
